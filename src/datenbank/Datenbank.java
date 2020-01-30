8169a3c20b06a82a0e6123c808f6a67b19037a2e
Sven <60364068+at-Sven@users.noreply.github.com>
2020-01-30 15:32:49 +0100

    Anfang Datenbank

---
+++ src/datenbank/Datenbank.java
@@ -0,0 +1,138 @@
+package datenbank;
+
+
+import com.sun.media.jfxmediaimpl.platform.Platform;
+import datenbank.connection.SQLiteDatenbankverbindung;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Klasse zur Steuerung der Datenbankvorgänge. Definiert als Singelton:
+ * https://de.wikibooks.org/wiki/Muster:_Java:_Singleton
+ */
+public class Datenbank extends SQLiteDatenbankverbindung {
+
+    private static Datenbank db;
+    private final static String DBFILE = "c:\\temp\\telefonbuch.sqlite";
+
+    /**
+     * private Konstruktor
+     */
+    private Datenbank() {
+        super(DBFILE);
+    }
+
+    /**
+     * Klassenmethode, die die Instanz der Datenbankklasse zurückgibt (Fabrikmethode genannt)
+     *
+     * @return Instanz der Datenbank-Klasse
+     */
+    public static Datenbank getInstance() {
+        if (db == null) {
+            // Neue Instanz von Datenbank erzeugen
+            db = new Datenbank();
+            db.init();
+        }
+
+        return db;
+    }
+
+    /**
+     * Diese Methode soll die erforderliche Datenstruktur der Datenbank herstellen
+     */
+    private void init() {
+        try {
+            ResultSet rs = executeQuery(SqlStatements.GET_VERSION);
+
+            int aktuelleDBVersion;
+            if (rs.next()) {
+                aktuelleDBVersion = rs.getInt(1);
+            } else {
+                aktuelleDBVersion = 0;
+            }
+            rs.close();
+
+            upgrade(aktuelleDBVersion);
+
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Fehler aufgetreten: " + e.getLocalizedMessage());
+            System.err.println("Programm wird geschlossen!");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Upgrade-Funktion der Datenbank, um bei einer neuen Programmversion gleich auch die Datenbank
+     * zu migrieren.
+     * @throws SQLException wird geworfen bei einem Fehler im Commit oder Rollback
+     */
+    private void upgrade(int oldDBVersion) throws SQLException {
+        if (oldDBVersion == SqlStatements.VERSION) {
+            System.out.println("Die Datenbank ist bereits aktuell.");
+            return;
+        } else if (oldDBVersion > SqlStatements.VERSION) {
+            throw new IllegalArgumentException("Die Datenbank ist neuer als die Version des Programms.\nProgramm wird beendet.");
+        } else {
+            System.out.println("Die Datenbank wird von Version " + oldDBVersion + " auf die Version " + SqlStatements.VERSION + " hochgestuft.");
+        }
+
+        boolean result = true;
+
+        switch(oldDBVersion) {
+            case 0: {
+                // Befehle für Upgrade auf Version 1...
+                result = execute(SqlStatements.CREATE_TABLE_USERS);
+
+                System.out.println("Upgrade auf DB-Version 1 durchgeführt.");
+                // KEIN break!!!! Wir wollen, dass die Datenbank immer auf die aktuelle Version upgedatet wird.
+            }
+            case 1: { //TODO: HIER WEITER
+                // Befehle für Upgrade auf Version 1
+/*                result = result && execute(SqlStatements.CREATE_TABLE_VORNAME);
+                result = result && execute(SqlStatements.CREATE_TABLE_NACHNAME);
+                result = result && execute(SqlStatements.CREATE_TABLE_STRASSE);
+                result = result && execute(SqlStatements.CREATE_TABLE_HAUSNR);
+                result = result && execute(SqlStatements.CREATE_TABLE_PLZ);
+                result = result && execute(SqlStatements.CREATE_TABLE_ORT);
+                result = result && execute(SqlStatements.CREATE_TABLE_TELEFONBUCH);
+                result = result && execute(SqlStatements.ACTIVATE_FOREIGN_KEYS);
+
+                result = result && execute(SqlStatements.INSERT_VORNAME);
+                result = result && execute(SqlStatements.INSERT_NACHNAME);
+                result = result && execute(SqlStatements.INSERT_STRASSE);
+                result = result && execute(SqlStatements.INSERT_HAUSNR);
+                result = result && execute(SqlStatements.INSERT_PLZ);
+                result = result && execute(SqlStatements.INSERT_ORT);
+                result = result && execute(SqlStatements.INSERT_TELFONBUCH);
+
+                result = result && execute(SqlStatements.DROP_TELEFONBUCH);
+
+                result = result && execute(SqlStatements.CREATE_VIEW_POSTS);
+                */
+
+                System.out.println("Upgrade auf DB-Version 2 durchgeführt.");
+                // KEIN break!!!! Wir wollen, dass die Datenbank immer auf die aktuelle Version upgedatet wird.
+            }
+            case 2: {
+                // Befehle für Upgrade auf Version 3...
+
+                // System.out.println("Upgrade auf DB-Version 3 durchgeführt.");
+                // KEIN break!!!! Wir wollen, dass die Datenbank immer auf die aktuelle Version upgedatet wird.
+            }
+        }
+
+        // Neue Datenbankversion setzen
+        result = result && execute(SqlStatements.SET_VERSION + SqlStatements.VERSION + ";");
+
+        // Wenn alles erfolgreich war und es keine Exception oder ein false gab,
+        // wird ein commit ausgeführt
+        if (result) {
+            commit();
+        } else {
+            rollback();
+        }
+    }
+}
---
+++ src/datenbank/connection/AllgemeineDatenbankverbindung.java
@@ -0,0 +1,150 @@
+package datenbank.connection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public abstract class AllgemeineDatenbankverbindung {
+
+    /**
+     * Connection-Objekt, welches die Datenbankverbindung enthält
+     */
+    protected Connection con;
+
+    /**
+     * Liste aller erzeugten PreparedStatements
+     */
+    private ArrayList<PreparedStatement> pstmtList;
+
+    /**
+     * Konstruktor
+     * Initialisiert die interne Liste der PreparedStatements
+     */
+    public AllgemeineDatenbankverbindung() {
+        pstmtList = new ArrayList<>();
+    }
+
+    /**
+     * Diese Methode dient der Erstellung der Verbindung zur Datenbank. Diese MUSS das
+     * Connection-Objekt 'con' korrekt setzen.
+     *
+     * Empfehlung: AutoCommit auf false setzen
+     *
+     * @return true bei erfolgreicher Verbindung zur Datenbank, false andernfalls.
+     */
+    public abstract boolean connect();
+
+    /**
+     * Trennt die Verbindung zur Datenbank.
+     */
+    public void disconnect() {
+        if (con != null) {
+
+            // Schließen aller PreparedStatements
+            for (PreparedStatement pstmt : pstmtList) {
+                try {
+                    pstmt.close();
+
+                    /* Sollte das Statement schon geschlossen sein, wird eine
+                     * Fehlermeldung geworfen. Diese wird ignoriert und mit dem
+                     * nächsten weitergemacht
+                     */
+                } catch (SQLException ignored) {}
+            }
+
+            // Löschen aller Referenzen zu den PreparedStatements
+            pstmtList.clear();
+
+            // Datenbankverbindung trennen
+            try {
+                con.close();
+            } catch (SQLException ignored) {
+            } finally {
+                con = null;
+            }
+        }
+    }
+
+    /**
+     * Führt ein Commit aus. Alle noch offenen Transaktionen werden in die Datenbank geschrieben.
+     *
+     * @throws SQLException wird geworfen, wenn ein Fehler beim Commit auftritt oder keine korrekte Verbindung zur Datenbank besteht
+     */
+    public void commit() throws SQLException {
+        if (con != null) {
+            con.commit();
+        }
+    }
+
+    /**
+     * Führt ein Rollback aus. Alle noch offenen Transaktionen werden rückgängig gemacht.
+     *
+     * @throws SQLException wird geworfen, wenn ein Fehler beim Rollback auftritt oder keine korrekte Verbindung zur Datenbank besteht
+     */
+    public void rollback() throws SQLException {
+        if (con != null) {
+            con.rollback();
+        }
+    }
+
+    /**
+     * Führt ein beliebiges SQL-Statement aus, und gibt etwaiige Fehler auf der Fehlerausgabe aus
+     * @param sql SQL-Statement welches ausgeführt werden soll
+     * @return gibt true zurück, wenn der Befehl ohne Fehler ausgeführt wurde, und false im Fehlerfall
+     */
+    public boolean execute(String sql) {
+        if (con == null) {
+            System.err.println("Datenbankverbindung geschlossen!");
+            return false;
+        } else {
+            try {
+                Statement stmt = con.createStatement();
+                stmt.execute(sql);
+                stmt.close();
+            } catch (SQLException e) {
+                System.err.println("Fehler beim Ausführen des folgenden Statements: " + sql + "\nFehlermeldung: " + e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Führt ein beliebiges SELECT-SQL-Statement aus und gibt das ResultSet zurück
+     * @param sql SQL-Statement welches ausgeführt werden soll
+     * @return ResultSet des Ergebnisses des SELECT-Statements
+     * @throws IllegalArgumentException wird geworfen, wenn das Statement nicht korrekt ausgeführt werden konnte
+     */
+    public ResultSet executeQuery(String sql) {
+        if (con == null) {
+            throw new IllegalArgumentException("Datenbankverbindung geschlossen!");
+        } else {
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                return rs;
+            } catch (SQLException e) {
+                throw new IllegalArgumentException("Fehler beim Ausführen des folgenden Statements: " + sql + "\nFehlermeldung: " + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Diese Methode bereitet den übergebenen SQL-Code als PreparedStatement vor und gibt das erzeugte Objekt zurück
+     * @param sql SQL-Statement
+     * @return PreparedStatementzu dem übergebenen SQL-Code
+     * @throws IllegalArgumentException wird geworfen, wenn das Statement nicht korrekt ausgeführt werden konnte
+     */
+    public PreparedStatement prepareStatement(String sql) {
+        if (con == null) {
+            throw new IllegalArgumentException("Datenbankverbindung geschlossen!");
+        } else {
+            try {
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                pstmtList.add(pstmt);
+                return pstmt;
+            } catch (SQLException e) {
+                throw new IllegalArgumentException("Fehler beim Ausführen des folgenden Statements: " + sql + "\nFehlermeldung: " + e.getMessage());
+            }
+        }
+    }
+}
